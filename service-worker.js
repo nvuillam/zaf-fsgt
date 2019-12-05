2c20aacf872e18eb0963102844e58e7dde735cb7
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2019-12-05 09:51:13 +0100

    Custom service worker

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+importScripts('./ngsw-worker.js');
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  if (clients.openWindow && event.notification.data.url) {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+  }
+});
